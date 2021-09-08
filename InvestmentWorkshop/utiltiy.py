8613847a7a6ac76db3841e33e83b7f40049b799f
Bruce Frank Wong <bruce.frank.wong@gmail.com>
2021-09-09 00:21:31 +0800

    Add <PACKAGE_PATH> and its tester.

--- InvestmentWorkshop/utiltiy.py
+++ InvestmentWorkshop/utiltiy.py
@@ -3,5 +3,12 @@
 __author__ = 'Bruce Frank Wong'
 
 
+from pathlib import Path
+
+
 # The package name.
 PACKAGE_NAME: str = 'InvestmentWorkshop'
+
+
+# The package path <InvestmentWorkshop>
+PACKAGE_PATH: Path = Path(__file__).parent
--- tests/test_utility.py
+++ tests/test_utility.py
@@ -3,11 +3,20 @@
 __author__ = 'Bruce Frank Wong'
 
 
+from pathlib import Path
+
+
 from InvestmentWorkshop.utiltiy import (
     PACKAGE_NAME,
+    PACKAGE_PATH,
 )
 
 
 def test_package_name():
     assert isinstance(PACKAGE_NAME, str)
     assert PACKAGE_NAME == 'InvestmentWorkshop'
+
+
+def test_package_path():
+    from InvestmentWorkshop import utiltiy
+    assert PACKAGE_PATH == Path(utiltiy.__file__).parent
