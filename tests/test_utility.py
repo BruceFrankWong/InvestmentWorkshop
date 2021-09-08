b4421dc2dbd31b8603b029a5a5dc04ffea5cfef0
Bruce Frank Wong <bruce.frank.wong@gmail.com>
2021-09-09 00:33:23 +0800

    Add <CONFIGS> and its tester.

--- tests/test_utility.py
+++ tests/test_utility.py
@@ -10,6 +10,7 @@ from InvestmentWorkshop.utility import (
     PACKAGE_NAME,
     PACKAGE_PATH,
     CONFIG_PATH,
+    CONFIGS,
 )
 
 
@@ -27,3 +28,12 @@ def test_config_path():
     config_path: Path = Path.home().joinpath(f'.{PACKAGE_NAME}')
     assert config_path.exists() is True
     assert config_path == CONFIG_PATH
+
+
+def test_configs():
+    assert isinstance(CONFIGS, dict) is True
+    assert 'path' in CONFIGS.keys()
+    assert 'http_header' in CONFIGS.keys()
+    assert 'database' in CONFIGS.keys()
+    assert 'SSIC' in CONFIGS.keys()
+    assert 'TQ' in CONFIGS.keys()
