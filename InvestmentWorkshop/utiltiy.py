e41288a4acdfd056847697184eb18d14a2dc8829
Bruce Frank Wong <bruce.frank.wong@gmail.com>
2021-09-08 23:09:32 +0800

    Add <PACKAGE_NAME> and its tester.

--- InvestmentWorkshop/utiltiy.py
+++ InvestmentWorkshop/utiltiy.py
@@ -1,3 +1,7 @@
 # -*- coding: UTF-8 -*-
 
 __author__ = 'Bruce Frank Wong'
+
+
+# The package name.
+PACKAGE_NAME: str = 'InvestmentWorkshop'
--- tests/test_utility.py
+++ tests/test_utility.py
@@ -1,3 +1,13 @@
 # -*- coding: UTF-8 -*-
 
 __author__ = 'Bruce Frank Wong'
+
+
+from InvestmentWorkshop.utiltiy import (
+    PACKAGE_NAME,
+)
+
+
+def test_package_name():
+    assert isinstance(PACKAGE_NAME, str)
+    assert PACKAGE_NAME == 'InvestmentWorkshop'
