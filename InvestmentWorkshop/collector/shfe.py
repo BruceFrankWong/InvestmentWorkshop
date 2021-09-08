4ee35f74eacd725eeb3fbc5b2229f70f9a4a41ad
Bruce Frank Wong <bruce.frank.wong@gmail.com>
2021-09-08 22:52:03 +0800

    Add <download_shfe_history_data> and its tester.

--- InvestmentWorkshop/collector/shfe.py
+++ InvestmentWorkshop/collector/shfe.py
@@ -1,3 +1,44 @@
 # -*- coding: UTF-8 -*-
 
 __author__ = 'Bruce Frank Wong'
+
+"""
+    collect public information from SHFE.
+
+    The earliest history data is begin:
+                                    Futures,    Option
+        Daily Express:              2002-01-07, 2018-09-21
+        Daily Rank:                 2002-01-07, N/A
+        Daily Warehouse Stocks:     2008-10-06, N/A
+"""
+
+
+from pathlib import Path
+import datetime as dt
+
+import requests
+
+
+def download_shfe_history_data(year: int, save_path: Path):
+    """
+    Download history data (yearly) from SHFE.
+    :param year:
+    :param save_path:
+    :return:
+    """
+    # Define the url for history data.
+    url: str = 'http://www.shfe.com.cn/historyData/MarketData_Year_{year:4d}.zip'
+
+    # Handle the year.
+    start_year: int = 2009
+    if year < start_year or year > dt.date.today().year:
+        raise ValueError(f'The year of SHFE history data should be in range {start_year} ~ {dt.date.today().year}.')
+
+    if not save_path.exists():
+        save_path.mkdir()
+
+    response = requests.get(url.format(year=year))
+    if response.status_code != 200:
+        raise requests.exceptions.HTTPError(f'Error in downloading <{url.format(year=year)}>.')
+    with open(save_path.joinpath(f'SHFE_{year:4d}.zip'), 'wb') as f:
+        f.write(response.content)
--- tests/test_collector_shfe.py
+++ tests/test_collector_shfe.py
@@ -1,3 +1,19 @@
 # -*- coding: UTF-8 -*-
 
 __author__ = 'Bruce Frank Wong'
+
+
+import pytest
+
+from pathlib import Path
+
+from InvestmentWorkshop.collector.shfe import (
+    download_shfe_history_data,
+)
+
+
+def test_download_shfe_history_data():
+    temp_path: Path = Path.cwd().parent.joinpath('temp')
+    year: int = 2021
+    download_shfe_history_data(year, temp_path)
+    assert temp_path.joinpath(f'SHFE_{year:4d}.zip').exists()
