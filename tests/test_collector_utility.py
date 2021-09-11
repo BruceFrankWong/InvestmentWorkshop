bd228f578b2742c4792965728faadc7d9defa950
Bruce Frank Wong <bruce.frank.wong@gmail.com>
2021-09-11 11:14:50 +0800

    Move <make_directory_existed> from <InvestmentWorkshop.collector.shfe> to <InvestmentWorkshop.collector.utility>.

--- tests/test_collector_utility.py
+++ tests/test_collector_utility.py
@@ -1,3 +1,35 @@
 # -*- coding: UTF-8 -*-
 
 __author__ = 'Bruce Frank Wong'
+
+
+from pathlib import Path
+
+from InvestmentWorkshop.collector.utility import (
+    make_directory_existed,
+)
+
+
+def test_make_directory_existed():
+    """
+    Test for <make_directory_existed>.
+    :return: None.
+    """
+    test_directory: Path = Path('E:\\Temporary')
+
+    # Make sure <test_directory> not existed.
+    if test_directory.exists():
+        # Remove files in <test_directory> if existed.
+        [file.unlink() for file in test_directory.iterdir()]
+        test_directory.rmdir()
+    assert test_directory.exists() is False
+
+    # Run <make_directory_existed>.
+    make_directory_existed(test_directory)
+
+    # Test.
+    assert test_directory.exists() is True
+
+    # Make clean.
+    test_directory.rmdir()
+    assert test_directory.exists() is False
