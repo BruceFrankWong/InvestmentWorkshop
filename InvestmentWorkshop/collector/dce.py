6bd5c9007ba97035d2c1ab74a191e059e3a5ed67
Bruce Frank Wong <bruce.frank.wong@gmail.com>
2021-09-11 22:10:29 +0800

    Add <fetch_dce_history_index> and its tester.

--- InvestmentWorkshop/collector/dce.py
+++ InvestmentWorkshop/collector/dce.py
@@ -1,3 +1,47 @@
 # -*- coding: UTF-8 -*-
 
 __author__ = 'Bruce Frank Wong'
+
+
+from typing import Dict, List
+import datetime as dt
+
+import requests
+from lxml import etree
+
+
+def fetch_dce_history_index() -> Dict[int, Dict[str, str]]:
+    """
+    Download history data (monthly) from DCE.
+    :return: Dict[int, Dict[str, str]].
+    """
+    result: Dict[int, Dict[str, str]] = {}
+    url_dce: str = 'http://www.dce.com.cn'
+    url: str = f'{url_dce}/dalianshangpin/xqsj/lssj/index.html'
+
+    # Make sure <year> in possible range.
+    year_list: List[int] = [year for year in range(2006, dt.date.today().year)]
+    year_list.reverse()
+    for year in year_list:
+        result[year] = {}
+
+    # Download index page.
+    response = requests.get(url)
+    if response.status_code != 200:
+        raise requests.exceptions.HTTPError(
+            f'Something wrong in downloading <{url}>.'
+        )
+    response.encoding = 'utf-8'
+
+    # Parse.
+    html: etree._Element = etree.HTML(response.text)
+    history_data_list: List[etree._Element] = html.xpath('//ul[@class="cate_sel clearfix"]')
+
+    for i in range(len(year_list)):
+        product_list = history_data_list[i].xpath('./li/label/text()')
+        url_list = history_data_list[i].xpath('./li/label/input/@rel')
+        assert len(product_list) == len(url_list)
+        for j in range(len(product_list)):
+            result[year_list[i]][product_list[j]] = f'{url_dce}/{url_list[j]}'
+
+    return result
--- tests/test_collector_dce.py
+++ tests/test_collector_dce.py
@@ -1,3 +1,23 @@
 # -*- coding: UTF-8 -*-
 
 __author__ = 'Bruce Frank Wong'
+
+
+import datetime as dt
+
+from InvestmentWorkshop.collector.dce import (
+    fetch_dce_history_index,
+)
+
+
+def test_fetch_dce_history_index():
+    result = fetch_dce_history_index()
+    assert isinstance(result, dict)
+    assert len(result) == (dt.date.today().year - 2006)
+
+    for year, content in result.items():
+        assert isinstance(year, int)
+        assert isinstance(content, dict)
+        for product, url in content.items():
+            assert isinstance(product, str)
+            assert isinstance(url, str)
