8dd689229d43a8a16b7196f58cb3cec3195b0474
Bruce Frank Wong <bruce.frank.wong@gmail.com>
2021-09-20 08:18:46 +0800

    Add <fetch_czce_history_index> and its test.

--- InvestmentWorkshop/collector/czce.py
+++ InvestmentWorkshop/collector/czce.py
@@ -1,3 +1,38 @@
 # -*- coding: UTF-8 -*-
 
 __author__ = 'Bruce Frank Wong'
+
+
+from typing import List, Dict
+
+import requests
+from lxml import etree
+
+
+def fetch_czce_history_index() -> Dict[str, List[str]]:
+    result: Dict[str, List[str]] = {
+        'futures': [],
+        'option': [],
+    }
+    url_czce: str = 'http://www.czce.com.cn'
+    url: str = f'{url_czce}/cn/jysj/lshqxz/H770319index_1.htm'
+
+    # Download index page.
+    response = requests.get(url)
+    if response.status_code != 200:
+        raise requests.exceptions.HTTPError(
+            f'Something wrong in downloading <{url}>.'
+        )
+    response.encoding = 'utf-8'
+
+    # Parse.
+    html: etree._Element = etree.HTML(response.text)
+    data_url_list: List[etree._Element] = html.xpath(
+        '//li/span[@class="hidden-xs"]/a[@target="_blank"]/@href'
+    )
+    for data_url in data_url_list:
+        if 'Option' in data_url:
+            result['option'].append(f'{url_czce}{data_url}')
+        else:
+            result['futures'].append(f'{url_czce}{data_url}')
+    return result
--- tests/test_collector_czce.py
+++ tests/test_collector_czce.py
@@ -1,3 +1,24 @@
 # -*- coding: UTF-8 -*-
 
 __author__ = 'Bruce Frank Wong'
+
+
+import pytest
+
+from InvestmentWorkshop.collector.czce import (
+    fetch_czce_history_index,
+)
+
+
+def test_fetch_czce_history_index():
+    result = fetch_czce_history_index()
+    assert isinstance(result, dict)
+
+    assert len(result.keys()) == 2
+    assert 'futures' in result.keys()
+    assert 'option' in result.keys()
+
+    for k, v in result.items():
+        assert isinstance(v, list)
+        for item in v:
+            assert isinstance(item, str)
