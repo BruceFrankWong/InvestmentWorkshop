cda6723ccf78707e64b0d871b011d51f67ff5bd6
Bruce Frank Wong <bruce.frank.wong@gmail.com>
2021-09-21 18:05:57 +0800

    Import statement for convenient.

--- InvestmentWorkshop/collector/__init__.py
+++ InvestmentWorkshop/collector/__init__.py
@@ -1,3 +1,35 @@
 # -*- coding: UTF-8 -*-
 
 __author__ = 'Bruce Frank Wong'
+
+
+from .utility import (
+    make_directory_existed,
+    unzip_quote_file,
+)
+from .shfe import (
+    download_shfe_history_data,
+    download_shfe_history_data_all,
+    read_shfe_history_data,
+)
+from .cffex import (
+    download_cffex_history_data,
+    download_cffex_history_data_all,
+    read_cffex_history_data,
+)
+from .dce import (
+    fetch_dce_history_index,
+    download_dce_history_data,
+    download_dce_history_data_all,
+    correct_format,
+    read_dce_history_data_csv,
+    read_dce_history_data_xls,
+    read_dce_history_data_xlsx,
+    read_dce_history_data,
+)
+from .czce import (
+    fetch_czce_history_index,
+    download_czce_history_data,
+    download_czce_history_data_all,
+    read_czce_history_data,
+)
