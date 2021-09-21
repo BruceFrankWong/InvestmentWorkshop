32ed8a9aecbc62082b7f89ec68ec53b757ad35bd
Bruce Frank Wong <bruce.frank.wong@gmail.com>
2021-09-22 00:32:04 +0800

    Add <create_postgresql_database> and its test.

--- tests/test_database_interface.py
+++ tests/test_database_interface.py
@@ -11,6 +11,7 @@ from InvestmentWorkshop.utility import PACKAGE_PATH
 from InvestmentWorkshop.database.interface import (
     create_sqlite_database,
     create_mysql_database,
+    create_postgresql_database,
 )
 
 
@@ -36,3 +37,19 @@ def test_create_mysql_database():
         }
     )
     assert isinstance(result, peewee.MySQLDatabase) is True
+
+
+def test_postgresql_database():
+    """
+    Test for create_postgresql_database.
+    """
+    result = create_postgresql_database(
+        {
+            'host': 'localhost',
+            'port': '5432',
+            'database': 'test_database',
+            'user': 'user',
+            'password': 'password'
+        }
+    )
+    assert isinstance(result, peewee.PostgresqlDatabase) is True
