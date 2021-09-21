2994534dc9a36f9dad5df461e4b8a9f45ab29424
Bruce Frank Wong <bruce.frank.wong@gmail.com>
2021-09-21 17:57:23 +0800

    Add <read_czce_history_data> and its test.

--- InvestmentWorkshop/collector/czce.py
+++ InvestmentWorkshop/collector/czce.py
@@ -3,8 +3,9 @@
 __author__ = 'Bruce Frank Wong'
 
 
-from typing import List, Dict
+from typing import List, Dict, Any
 from pathlib import Path
+import datetime as dt
 
 import requests
 from lxml import etree
@@ -98,3 +99,102 @@ def download_czce_history_data_all():
                 raise requests.exceptions.HTTPError(f'Error in downloading <{url.format(year=year)}>.')
             with open(download_path.joinpath(f'CZCE_{type_}_{year:4d}.zip'), 'wb') as f:
                 f.write(response.content)
+
+
+def read_czce_history_data(data_file: Path) -> List[Dict[str, Any]]:
+    assert data_file.exists() is True
+
+    result: List[Dict[str, Any]] = []
+
+    try:
+        with open(data_file, mode='r', encoding='gbk') as txt_file:
+            lines = txt_file.readlines()
+    except UnicodeDecodeError:
+        try:
+            with open(data_file, mode='r', encoding='utf-8') as txt_file:
+                lines = txt_file.readlines()
+        except UnicodeDecodeError:
+            raise
+
+    for line in lines[2:]:
+        data = line.split('|')
+        data = [x.strip() for x in data]
+        if len(data[0]) == 0:
+            break
+        if 'option' in data_file.stem:
+            result.append(
+                {
+                    # 交易日期
+                    'date': dt.date.fromisoformat(data[0]),
+                    # 合约代码
+                    'symbol': data[1],
+                    # 昨结算
+                    'previous_settlement': float(data[2].replace(',', '')),
+                    # 今开盘
+                    'open': None if data[3] == '0.00' else float(data[3].replace(',', '')),
+                    # 最高价
+                    'high': None if data[4] == '0.00' else float(data[4].replace(',', '')),
+                    # 最低价
+                    'low': None if data[5] == '0.00' else float(data[5].replace(',', '')),
+                    # 今收盘
+                    'close': float(data[6].replace(',', '')),
+                    # 今结算
+                    'settlement': float(data[7].replace(',', '')),
+                    # 涨跌1
+                    'change_on_close': float(data[8].replace(',', '')),
+                    # 涨跌2
+                    'change_on_settlement': float(data[9].replace(',', '')),
+                    # 成交量(手)
+                    'volume': int(data[10].replace(',', '')),
+                    # 持仓量
+                    'open_interest': int(data[11].replace(',', '')),
+                    # 增减量
+                    'change_on_open_interest': int(data[12].replace(',', '')),
+                    # 成交额(万元)
+                    'amount': float(data[13].replace(',', '')) * 10000,
+                    # DELTA
+                    'delta': float(data[14].replace(',', '')),
+                    # 隐含波动率
+                    'implied_volatility': float(data[15].replace(',', '')),
+                    # 行权量
+                    'exercise': int(data[16].replace(',', '')),
+                }
+            )
+        else:
+            result.append(
+                {
+                    # 交易日期
+                    'date': dt.date.fromisoformat(data[0]),
+                    # 合约代码
+                    'symbol': data[1],
+                    # 昨结算
+                    'previous_settlement': float(data[2].replace(',', '')),
+                    # 今开盘
+                    'open': None if data[3] == '0.00' else float(data[3].replace(',', '')),
+                    # 最高价
+                    'high': None if data[4] == '0.00' else float(data[4].replace(',', '')),
+                    # 最低价
+                    'low': None if data[5] == '0.00' else float(data[5].replace(',', '')),
+                    # 今收盘
+                    'close': float(data[6].replace(',', '')),
+                    # 今结算
+                    'settlement': float(data[7].replace(',', '')),
+                    # 涨跌1
+                    'change_on_close': float(data[8].replace(',', '')),
+                    # 涨跌2
+                    'change_on_settlement': float(data[9].replace(',', '')),
+                    # 成交量(手)
+                    'volume': int(data[10].replace(',', '')),
+                    # 持仓量
+                    'open_interest': int(data[11].replace(',', '')),
+                    # 增减量
+                    'change_on_open_interest': int(data[12].replace(',', '')),
+                    # 成交额(万元)
+                    'amount': float(data[13].replace(',', '')) * 10000,
+                    # 交割结算价
+                    'delivery_settlement': None
+                    if data[14] == '0.00' or len(data[14]) == 0 else float(data[14].replace(',', '')),
+                }
+            )
+
+    return result
--- tests/test_collector_czce.py
+++ tests/test_collector_czce.py
@@ -11,10 +11,12 @@ import datetime as dt
 import random
 
 from InvestmentWorkshop.utility import CONFIGS
+from InvestmentWorkshop.collector.utility import unzip_quote_file
 from InvestmentWorkshop.collector.czce import (
     fetch_czce_history_index,
     download_czce_history_data,
     download_czce_history_data_all,
+    read_czce_history_data,
 )
 
 
@@ -145,3 +147,105 @@ def test_download_czce_history_data_all(download_path):
         assert download_file.exists() is True
         download_file.unlink()
         assert download_file.exists() is False
+
+
+def test_read_czce_history_data(download_path):
+    url_mapper = fetch_czce_history_index()
+    download_file_list: List[Path] = []
+    for year in url_mapper['futures'].keys():
+        download_file_list.append(download_path.joinpath(f'CZCE_futures_{year:4d}.zip'))
+    for year in url_mapper['option'].keys():
+        download_file_list.append(download_path.joinpath(f'CZCE_option_{year:4d}.zip'))
+
+    # Make sure <download_file_list> not existed.
+    for download_file in download_file_list:
+        if download_file.exists():
+            download_file.unlink()
+        assert download_file.exists() is False
+
+    download_czce_history_data_all()
+
+    # Unzip.
+    unzipped_file_list: List[Path] = []
+    for download_file in download_file_list:
+        assert download_file.exists() is True
+        unzipped_list = unzip_quote_file(download_file)
+        if len(unzipped_list) == 1:
+            unzipped_file = unzipped_list[0]
+            new_name = download_path.joinpath(f'{download_file.stem}.txt')
+            unzipped_file.rename(new_name)
+            assert new_name.exists() is True
+            unzipped_file_list.append(new_name)
+            download_file.unlink()
+            assert download_file.exists() is False
+
+    # Test read.
+    for unzipped_file in unzipped_file_list:
+        result = read_czce_history_data(unzipped_file)
+        assert isinstance(result, list)
+        for item in result:
+            assert isinstance(item, dict)
+            # 交易日期
+            assert 'date' in item.keys()
+            assert isinstance(item['date'], dt.date)
+            # 合约代码
+            assert 'symbol' in item.keys()
+            assert isinstance(item['symbol'], str)
+            # 昨结算
+            assert 'previous_settlement' in item.keys()
+            assert isinstance(item['previous_settlement'], float)
+            # 今开盘
+            assert 'open' in item.keys()
+            if item['open'] is not None:
+                assert isinstance(item['open'], float)
+            # 最高价
+            assert 'high' in item.keys()
+            if item['high'] is not None:
+                assert isinstance(item['high'], float)
+            # 最低价
+            assert 'low' in item.keys()
+            if item['low'] is not None:
+                assert isinstance(item['low'], float)
+            # 今收盘
+            assert 'close' in item.keys()
+            assert isinstance(item['close'], float)
+            # 今结算
+            assert 'close' in item.keys()
+            assert isinstance(item['close'], float)
+            # 涨跌1
+            assert 'change_on_close' in item.keys()
+            assert isinstance(item['change_on_close'], float)
+            # 涨跌2
+            assert 'change_on_settlement' in item.keys()
+            assert isinstance(item['change_on_settlement'], float)
+            # 成交量(手)
+            assert 'volume' in item.keys()
+            assert isinstance(item['volume'], int)
+            # 持仓量
+            assert 'open_interest' in item.keys()
+            assert isinstance(item['open_interest'], int)
+            # 增减量
+            assert 'change_on_open_interest' in item.keys()
+            assert isinstance(item['change_on_open_interest'], int)
+            # 成交额(万元)
+            assert 'amount' in item.keys()
+            assert isinstance(item['amount'], float)
+
+            if 'option' in unzipped_file.stem:
+                # DELTA
+                assert 'delta' in item.keys()
+                assert isinstance(item['delta'], float)
+                # 隐含波动率
+                assert 'implied_volatility' in item.keys()
+                assert isinstance(item['implied_volatility'], float)
+                # 行权量
+                assert 'exercise' in item.keys()
+                assert isinstance(item['exercise'], int)
+            else:
+                # 交割结算价
+                assert 'delivery_settlement' in item.keys()
+                if item['delivery_settlement'] is not None:
+                    assert isinstance(item['delivery_settlement'], float)
+
+        unzipped_file.unlink()
+        assert unzipped_file.exists() is False
