bd5978e55a7cfceedba7ffb93c27b5331b799b54
Bruce Frank Wong <bruce.frank.wong@gmail.com>
2021-09-22 00:23:37 +0800

    Add <create_sqlite_database> and its test.

--- InvestmentWorkshop/database/interface.py
+++ InvestmentWorkshop/database/interface.py
@@ -1,3 +1,23 @@
 # -*- coding: UTF-8 -*-
 
 __author__ = 'Bruce Frank Wong'
+
+
+from typing import Dict
+from pathlib import Path
+
+from peewee import (
+    SqliteDatabase,
+)
+
+
+def create_sqlite_database(settings: Dict[str, str]) -> SqliteDatabase:
+    return SqliteDatabase(
+        Path(settings['database']),
+        pragmas={
+            'journal_mode': 'wal',
+            'encoding': 'utf8',
+            'cache_size': -640000,  # 64MB
+            'foreign_keys': 1,  # Enforce foreign-key constraints
+        }
+    )
--- tests/test_database_interface.py
+++ tests/test_database_interface.py
@@ -1,3 +1,19 @@
 # -*- coding: UTF-8 -*-
 
 __author__ = 'Bruce Frank Wong'
+
+
+from typing import Dict
+
+import peewee
+
+from InvestmentWorkshop.utility import PACKAGE_PATH
+from InvestmentWorkshop.database.interface import create_sqlite_database
+
+
+def test_create_sqlite_database():
+    parameter: Dict[str, str] = {
+        'database': str(PACKAGE_PATH.joinpath('test_database.sqlite'))
+    }
+    result = create_sqlite_database(parameter)
+    assert isinstance(result, peewee.SqliteDatabase) is True
