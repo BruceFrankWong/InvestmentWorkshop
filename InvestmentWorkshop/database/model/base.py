55c4fff63f1041833f80452e7eaf054c008f7f73
Bruce Frank Wong <bruce.frank.wong@gmail.com>
2021-09-22 02:03:13 +0800

    Add the base model <BasicModel>.

--- InvestmentWorkshop/database/model/base.py
+++ InvestmentWorkshop/database/model/base.py
@@ -1,3 +1,18 @@
 # -*- coding: UTF-8 -*-
 
 __author__ = 'Bruce Frank Wong'
+
+
+from peewee import Model
+
+from ..interface import db
+
+
+class BasicModel(Model):
+    """
+    The base of all the models.
+    It declares the database connection, and the table name style.
+    """
+    class Meta:
+        database = db
+        legacy_table_names = False
