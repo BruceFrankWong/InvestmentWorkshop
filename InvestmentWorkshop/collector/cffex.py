8bcff1f5ba6f4aa2ac6561ea337e06389ebf563d
Bruce Frank Wong <bruce.frank.wong@gmail.com>
2021-10-18 09:04:51 +0800

    Refactor.

--- InvestmentWorkshop/collector/cffex.py
+++ InvestmentWorkshop/collector/cffex.py
@@ -3,123 +3,219 @@
 __author__ = 'Bruce Frank Wong'
 
 
-from typing import List
+"""
+    collect public information from CFFEX.
+
+"""
+
+
+from typing import Dict, List, Tuple, Any
 from pathlib import Path
 import datetime as dt
 import csv
 
 import requests
 
-from ..utility import CONFIGS
-from .utility import make_directory_existed, QuoteDaily, split_symbol
+from .utility import split_symbol
 
 
-def download_cffex_history_data(month_required: dt.date) -> None:
+# 常量定义
+
+# 中金所历史数据开始提供日期
+CFFEX_HISTORY_DATA_START_YEAR: int = 2010
+CFFEX_HISTORY_DATA_START_MONTH: int = 4
+CFFEX_HISTORY_DATA_START_DAY: int = 16
+
+# 中金所交易代码的正则表达式
+CFFEX_PATTERN_FUTURES: str = r'([A-Z]{1,2})([0-9]{3,4})'
+CFFEX_PATTERN_OPTION: str = r'([A-Z]{1,2})([0-9]{3,4})\-([CP])\-([0-9]+)'
+
+
+def check_cffex_parameter(year: int, month: int) -> None:
     """
-    Download history data (monthly) from CFFEX.
-    :param month_required:
+    校验参数<year>（年份）、<month>（月份）的有效性，无效抛出异常。
+
+    :param year:  int，年份。
+    :param month: int，月份。
     :return:
     """
-    url: str = 'http://www.cffex.com.cn/sj/historysj/{year:4d}{month:02d}/zip/{year:4d}{month:02d}.zip'
-
-    # Make sure <year> in possible range.
-    start_year: int = 2010
-    start_month: int = 4
+    # 今天的日期。
     today: dt.date = dt.date.today()
 
-    # Make sure <year> and <month> in possible range.
-    year: int = month_required.year
-    month: int = month_required.month
-    if year < start_year or (year == start_year and month < start_month):
-        raise ValueError(f'CFFEX history data is begin from {start_year}={start_month}.')
+    # 如果 <month> 小于 1 或者大于 12，抛出异常。
+    if month < 1 or month > 12:
+        raise ValueError(f'参数 <month> 取值范围在 [1, 12]。')
+
+    # 如果 <year> 与 <month> 早于 HISTORY_DATA_START_YEAR 与 HISTORY_DATA_START_MONTH，抛出异常。
+    if year < CFFEX_HISTORY_DATA_START_YEAR or (year == CFFEX_HISTORY_DATA_START_YEAR and month < CFFEX_HISTORY_DATA_START_MONTH):
+        raise ValueError(f'中金所历史数据自{CFFEX_HISTORY_DATA_START_YEAR:4d}年{CFFEX_HISTORY_DATA_START_MONTH:02d}月起开始提供。')
+
+    # 如果 <year> 与 <month> 晚于当前年月，抛出异常。
     if year > today.year or (year == today.year and month > today.month):
-        raise ValueError(f'The data your required is not existed.')
-
-    # Handle <save_path>.
-    download_path: Path = Path(CONFIGS['path']['download'])
-    make_directory_existed(download_path)
-
-    # Do download.
-    response = requests.get(url.format(year=year, month=month))
-    if response.status_code != 200:
-        raise requests.exceptions.HTTPError(
-            f'Something wrong in downloading <{url.format(year=year, month=month)}>.'
-        )
-
-    # Write to file.
-    with open(download_path.joinpath(f'CFFEX_{year:4d}-{month:02d}.zip'), 'wb') as f:
-        f.write(response.content)
+        raise ValueError(f'{year:4d}年{month:02d}月是未来日期。')
 
 
-def download_cffex_history_data_all() -> None:
+def get_all_cffex_history_data_parameters() -> List[Tuple[int, int]]:
     """
-    Download all history data (monthly) from CFFEX.
-    :return:
+    返回全部中金所历史数据的参数列表。
+
+    :return: 一个 list，每一项都是一个 tuple。tuple 有两项，均为 int，前者是年份，后者是月份。
     """
-    start_year: int = 2010
-    start_month: int = 4
     today: dt.date = dt.date.today()
 
-    require_list: List[dt.date] = []
-    for year in range(start_year, today.year + 1):
+    result: List[Tuple[int, int]] = []
+    for year in range(CFFEX_HISTORY_DATA_START_YEAR, today.year + 1):
         for month in range(1, 12 + 1):
-            if year == 2010 and month < start_month:
+            if year == CFFEX_HISTORY_DATA_START_YEAR and month < CFFEX_HISTORY_DATA_START_MONTH:
                 continue
             if year == today.year and month > today.month:
                 break
-            require_list.append(dt.date(year=year, month=month, day=1))
-    for item in require_list:
-        download_cffex_history_data(item)
+            result.append((year, month))
+
+    return result
 
 
-def read_cffex_history_data(csv_file: Path) -> List[QuoteDaily]:
+def get_cffex_history_data_local_filename(year: int, month: int) -> str:
     """
-    Read quote data from file (csv).
-    :param csv_file:
-    :return:
-    """
-    result: List[QuoteDaily] = []
+    返回中金所历史数据文件的本地文件名字符串，避免在项目各处硬编码中金所历史数据文件名（或文件名模板）。
 
-    # Handle date.
-    filename: str = csv_file.name[:8]
+    参数<year>（年份）、<month>（月份）会经过校验，不再有效范围中将抛出异常。
+
+    :param year:  int, 数据年份。
+    :param month: int, 数据月份
+    :return: str, local filename.
+    """
+    # 确认参数有效。
+    check_cffex_parameter(year=year, month=month)
+    return f'CFFEX_{year:4d}-{month:02d}.zip'
+
+
+def download_cffex_history_data(save_path: Path, year: int, month: int) -> None:
+    """
+    下载中国金融期货交易所（中金所，CFFEX）的历史数据。
+
+    :param save_path: Path，保存的位置。
+    :param year: int，需要下载数据的年份。
+    :param month: int，需要下载数据的月份。
+    :return: None.
+    """
+
+    # 中金所历史数据 url 模板。
+    url_pattern: str = 'http://www.cffex.com.cn/sj/historysj/{year:4d}{month:02d}/zip/{year:4d}{month:02d}.zip'
+
+    # 确认参数有效。
+    check_cffex_parameter(year=year, month=month)
+
+    # 如果参数 <save_path> 不存在，引发异常。
+    if not save_path.exists():
+        raise FileNotFoundError(f'目录 {save_path} 不存在。')
+
+    # 下载。
+    url: str = url_pattern.format(year=year, month=month)
+    response = requests.get(url)
+
+    # 如果下载不顺利，引发异常。
+    if response.status_code != 200:
+        raise requests.exceptions.HTTPError(f'下载 <{url}> 时发生错误。')
+
+    # 保存文件。
+    with open(save_path.joinpath(
+            get_cffex_history_data_local_filename(year=year, month=month)
+    ), 'wb') as f:
+        f.write(response.content)
+
+
+def read_cffex_history_data(data_file: Path) -> Tuple[List[Dict[str, Any]], List[Dict[str, Any]]]:
+    """
+    读取中国金融期货交易所（中金所，CFFEX）的历史交易数据 (csv 文件)。
+
+    :param data_file: Path, 待读取的文件。
+    :return: tuple, 共两项, 每一项都是一个 list，前者是期货数据, 后者是期权数据。
+             list 的每一项都是一个 dict。
+             dict 的 key 是 str 类型, value 是任意类型。
+    """
+    result_futures: List[Dict[str, Any]] = []
+    result_option: List[Dict[str, Any]] = []
+
+    # 从文件名中获得日期。
+    filename: str = data_file.name[:8]
     date: dt.date = dt.date(
         year=int(filename[:4]),
         month=int(filename[4:6]),
         day=int(filename[6:8])
     )
 
-    # Read .csv files.
-    with open(csv_file, mode='r', encoding='gbk') as csv_file:
+    # 打开 <data_file> 读取数据。
+    with open(data_file, mode='r', encoding='gbk') as csv_file:
         reader = csv.DictReader(csv_file)
+
+        # 按行循环读取。
         for row in reader:
-            if row['合约代码'] == '小计' or row['合约代码'] == '合计' or '-' in row['合约代码']:
+            # 忽略 <合约代码> 列为 <小计>、<合计> 的行。
+            if row['合约代码'] == '小计' or row['合约代码'] == '合计':
                 continue
+
+            # 捕捉异常并打印出错的 row。
             try:
+                # 合约代码，去除两端的空白（空格）
                 symbol = row['合约代码'].strip()
-                product, contract = split_symbol(symbol)
-                result.append(
-                    {
-                        'exchange': 'CFFEX',
-                        'date': date,
-                        'symbol': symbol,
-                        'product': product,
-                        'contract': contract,
-                        'open': float(row['今开盘']) if len(row['今开盘']) > 0 else 0.0,
-                        'high': float(row['最高价']) if len(row['最高价']) > 0 else 0.0,
-                        'low': float(row['最低价']) if len(row['最低价']) > 0 else 0.0,
-                        'close': float(row['今收盘']) if len(row['今收盘']) > 0 else 0.0,
-                        'settlement': float(row['今结算']),
-                        'previous_settlement': float(row['前结算']),
-                        'volume': int(row['成交量']) if len(row['成交量']) > 0 else 0,
-                        'amount': float(row['成交金额']) if len(row['成交金额']) > 0 else 0.0,
-                        'open_interest': int(float(row['持仓量'])),
-                        'change_on_close': float(row['涨跌1']),
-                        'change_on_settlement': float(row['涨跌2']),
-                        'change_on_open_interest': int(float(row['持仓变化'])),
-                    }
-                )
-            except ValueError as e:
-                print(f'ERROR: in file {csv_file}.')
-                print(f'Content: \n\t{row}.')
-                print(e)
-    return result
+
+                # 合约代码长度不超过6，期货
+                if len(symbol) <= 6:
+                    # 分解代码
+                    symbol_tuple = split_symbol(symbol, CFFEX_PATTERN_FUTURES)
+
+                    result_futures.append(
+                        {
+                            'exchange': 'CFFEX',
+                            'date': date,
+                            'symbol': symbol,
+                            'product': symbol_tuple[0],
+                            'expiration': symbol_tuple[1],
+                            'open': float(row['今开盘']) if len(row['今开盘']) > 0 else 0.0,
+                            'high': float(row['最高价']) if len(row['最高价']) > 0 else 0.0,
+                            'low': float(row['最低价']) if len(row['最低价']) > 0 else 0.0,
+                            'close': float(row['今收盘']) if len(row['今收盘']) > 0 else 0.0,
+                            'settlement': float(row['今结算']),
+                            'previous_settlement': float(row['前结算']),
+                            'volume': int(row['成交量']) if len(row['成交量']) > 0 else 0,
+                            'amount': float(row['成交金额']) if len(row['成交金额']) > 0 else 0.0,
+                            'open_interest': int(float(row['持仓量'])),
+                            'change_on_close': float(row['涨跌1']),
+                            'change_on_settlement': float(row['涨跌2']),
+                            'change_on_open_interest': int(float(row['持仓变化'])),
+                        }
+                    )
+                # 合约代码长度超过6，期权
+                else:
+                    # 分解代码
+                    symbol_tuple = split_symbol(symbol, CFFEX_PATTERN_OPTION)
+
+                    result_option.append(
+                        {
+                            'exchange': 'CFFEX',
+                            'date': date,
+                            'symbol': symbol,
+                            'product': symbol_tuple[0],
+                            'expiration': symbol_tuple[1],
+                            'offset': symbol_tuple[2],
+                            'exercise_price': symbol_tuple[3],
+                            'open': float(row['今开盘']) if len(row['今开盘']) > 0 else 0.0,
+                            'high': float(row['最高价']) if len(row['最高价']) > 0 else 0.0,
+                            'low': float(row['最低价']) if len(row['最低价']) > 0 else 0.0,
+                            'close': float(row['今收盘']) if len(row['今收盘']) > 0 else 0.0,
+                            'settlement': float(row['今结算']),
+                            'previous_settlement': float(row['前结算']),
+                            'volume': int(row['成交量']) if len(row['成交量']) > 0 else 0,
+                            'amount': float(row['成交金额']) if len(row['成交金额']) > 0 else 0.0,
+                            'open_interest': int(float(row['持仓量'])),
+                            'change_on_close': float(row['涨跌1']),
+                            'change_on_settlement': float(row['涨跌2']),
+                            'change_on_open_interest': int(float(row['持仓变化'])),
+                            'delta': 0.0 if row['Delta'] == '--' else float(row['Delta']),
+                        }
+                    )
+            except ValueError:
+                print(f'读取文件 {csv_file} 时发生错误。发生错误的行内容为：\n\t{row}')
+
+    return result_futures, result_option
--- tests/test_collector_cffex.py
+++ tests/test_collector_cffex.py
@@ -5,176 +5,260 @@ __author__ = 'Bruce Frank Wong'
 
 import pytest
 
-from typing import List
+from typing import Dict, List, Tuple, Any
 from pathlib import Path
 import datetime as dt
-import random
+import os.path
 
-from InvestmentWorkshop.utility import CONFIGS
-from InvestmentWorkshop.collector.utility import unzip_file
 from InvestmentWorkshop.collector.cffex import (
+    CFFEX_HISTORY_DATA_START_YEAR,
+    CFFEX_HISTORY_DATA_START_MONTH,
+    check_cffex_parameter,
+    get_all_cffex_history_data_parameters,
+    get_cffex_history_data_local_filename,
     download_cffex_history_data,
-    download_cffex_history_data_all,
     read_cffex_history_data,
 )
+from InvestmentWorkshop.collector.utility import uncompress_zip_file
 
 
-@pytest.fixture()
-def download_path() -> Path:
+@pytest.mark.parametrize(
+    'year, month',
+    [
+        (dt.date.today().year, dt.date.today().month),  # 当前测试运行时间的年、月
+        (2010, 4),      # 数据开始提供的年、月
+        (2019, 8),      # 常规日期
+        (2008, 12),     # 早于有效时间范围，应抛出异常
+        (2030, 3),      # 晚于有效时间范围，应抛出异常
+        (2020, 15),     # 月份数字无效，应抛出异常
+        (2020, -3),     # 月份数字无效，应抛出异常
+    ]
+)
+def test_check_cffex_parameter(year, month):
     """
-    Return download path which configured in <CONFIGS>.
-    :return: a Path-like object.
+    测试 collector.cffex.check_cffex_parameter()。
+
+    中金所的数据从2010年4月开始提供（股指期货上市），至当前测试时间的年份、月份。如果参数无效，抛出 ValueError 异常。
+
+    :param year:  int，待下载数据的年份。
+    :param month: int，待下载数据的月份。
+    :return: None。
     """
-    return Path(CONFIGS['path']['download'])
-
-
-@pytest.fixture()
-def download_date() -> dt.date:
-    """
-    Generate a random year and month to download.
-    :return: int.
-    """
-    today: dt.date = dt.date.today()
-    year: int = random.randint(2010, today.year)
-    month: int
-    if year == 2010:
-        month = random.randint(4, 12)
-    elif year == today.year:
-        month = random.randint(1, today.month)
-    else:
-        month = random.randint(1, 12)
-    return dt.date(
-        year=year,
-        month=month,
-        day=1
-    )
-
-
-def test_download_cffex_history_data(download_path, download_date):
-    """
-    Test <InvestmentWorkshop.collector.shfe.download_shfe_history_data(year: int, save_path: Path)>.
-
-    :param download_path: Test fixture. A Path-like object, where tester save downloaded files.
-    :param download_date: Test fixture. A datetime.date object, which month to be downloaded to test.
-    :return: None
-    """
-    # Test for normal.
-    year: int = download_date.year
-    month: int = download_date.month
-
-    download_file: Path = download_path.joinpath(f'CFFEX_{year:4d}-{month:02d}.zip')
-    if download_file.exists():
-        download_file.unlink()
-    assert download_file.exists() is False
-
-    download_cffex_history_data(download_date)
-    assert download_file.exists() is True
-
-    # Test for impossible date.
-    with pytest.raises(ValueError):
-        download_cffex_history_data(dt.date(year=1999, month=5, day=1))
-
-    # make clean.
-    download_file.unlink()
-    assert download_file.exists() is False
-
-
-def test_download_cffex_history_data_all():
-    """
-    Test for <download_cffex_history_data_all>.
-    :return:
-    """
-    # Generate files.
-    start_year: int = 2010
-    start_month: int = 4
+    # 当前日期
     today: dt.date = dt.date.today()
 
-    download_path = Path(CONFIGS['path']['download'])
-    file_list: List[Path] = []
-    require_list: List[dt.date] = []
-    for year in range(start_year, today.year + 1):
-        for month in range(1, 12 + 1):
-            if year == 2010 and month < start_month:
-                continue
-            if year == today.year and month > today.month:
-                break
-            require_list.append(dt.date(year=year, month=month, day=1))
-            file_list.append(
-                download_path.joinpath(
-                    f'CFFEX_{year:4d}-{month:02d}.zip'
-                )
-            )
+    # 如果给定的月份数字无效（小于 1 或者大于 12），应该抛出异常。
+    if month < 1 or month > 12:
+        with pytest.raises(ValueError):
+            check_cffex_parameter(year, month)
 
-    # Make sure file not existed.
-    for download_file in file_list:
-        if download_file.exists():
-            download_file.unlink()
-        assert download_file.exists() is False
+    # 如果给定的年份数字超出时间范围，应该抛出异常。
+    elif year < CFFEX_HISTORY_DATA_START_YEAR or year > today.year:
+        with pytest.raises(ValueError):
+            check_cffex_parameter(year, month)
 
-    # Download.
-    download_cffex_history_data_all()
-
-    # Assert download succeed.
-    for download_file in file_list:
-        assert download_file.exists() is True
-
-    # make clean.
-    for download_file in file_list:
-        download_file.unlink()
-        assert download_file.exists() is False
+    # 如果给定的年份、月份数字组合起来超出时间范围（指的是2010年1~3月，当前运行测试的年份中剩余的月份），应该抛出异常。
+    elif (year == CFFEX_HISTORY_DATA_START_YEAR and month < CFFEX_HISTORY_DATA_START_MONTH) or (
+            year == today.year and month > today.month):
+        with pytest.raises(ValueError):
+            check_cffex_parameter(year, month)
 
 
-def test_read_cffex_history_data(download_date, download_path):
+def test_get_all_cffex_history_data_parameters():
     """
-    Test for <read_cffex_history_data>.
-    :return:
+    测试 collector.cffex.get_all_cffex_history_data_parameters()。
+
+    get_all_cffex_history_data_parameters() 应返回一个 list，其中每一项均为一个 tuple。
+    tuple 有两项，均为 int，前者是年份，后者是月份。
+
+    :return: None。
     """
-    # Generate a random CFFEX history data file.
-    year: int = download_date.year
-    month: int = download_date.month
+    result = get_all_cffex_history_data_parameters()
+    assert isinstance(result, list)
 
-    # Assert <download_file> not existed.
-    download_file: Path = download_path.joinpath(f'CFFEX_{year:4d}-{month:02d}.zip')
-    assert download_file.exists() is False
+    # 排序
+    result.sort(key=lambda x: (x[0], x[1]))
 
-    # Download.
-    download_cffex_history_data(download_date)
+    today: dt.date = dt.date.today()
 
-    # Assert <download_file> existed.
-    assert download_file.exists() is True
+    assert result[0][0] == CFFEX_HISTORY_DATA_START_YEAR and result[0][1] == CFFEX_HISTORY_DATA_START_MONTH
+    assert result[-1][0] == today.year and result[-1][1] == today.month
 
-    # Unzip <download_file>.
-    unzip_file_list: List[Path] = list(unzip_file(download_file))
-    csv_file: Path
-    for csv_file in unzip_file_list:
-        assert csv_file.exists() is True
+    count: int = 9          # HISTORY_DATA_START_YEAR (2010) 年，4月~12月，共9个。
+    count += today.month    # 当前年份的数量，至当前月份。
+    count += (today.year - CFFEX_HISTORY_DATA_START_YEAR - 1) * 12    # 中间相差的年份，每一年都有12个月。
+    assert len(set(result)) == count
 
-        result = read_cffex_history_data(csv_file)
-        assert isinstance(result, list)
-        for item in result:
-            assert isinstance(item, dict)
-            assert isinstance(item['symbol'], str)
-            assert isinstance(item['product'], str)
-            assert len(item['product']) <= 2
-            assert isinstance(item['contract'], str)
-            assert len(item['contract']) == 4
-            assert isinstance(item['date'], dt.date)
-            assert isinstance(item['previous_settlement'], float)
-            assert isinstance(item['open'], float)
-            assert isinstance(item['high'], float)
-            assert isinstance(item['low'], float)
-            assert isinstance(item['close'], float)
-            assert isinstance(item['settlement'], float)
-            assert isinstance(item['change_on_close'], float)
-            assert isinstance(item['change_on_settlement'], float)
-            assert isinstance(item['volume'], int)
-            assert isinstance(item['amount'], float)
-            assert isinstance(item['open_interest'], int)
-            assert isinstance(item['change_on_open_interest'], int)
 
-    # make clean.
-    for csv_file in unzip_file_list:
-        csv_file.unlink()
-        assert csv_file.exists() is False
-    download_file.unlink()
-    assert download_file.exists() is False
+def test_get_cffex_history_data_local_filename():
+    """
+    测试 collector.cffex.get_cffex_history_data_local_filename()。
+
+    get_cffex_history_data_local_filename() 应返回一个 str，长度为 17。
+    格式为 <CFFEX_{year:4d}-{month:02d}.zip>。
+
+    :return: None。
+    """
+    for parameter in get_all_cffex_history_data_parameters():
+        year: int = parameter[0]
+        month: int = parameter[1]
+        result = get_cffex_history_data_local_filename(year, month)
+        assert isinstance(result, str)
+        assert len(result) == 17
+        assert result[:6] == 'CFFEX_'
+        assert result[6:10] == str(f'{year:4d}')
+        assert result[10] == '-'
+        assert result[11:13] == str(f'{month:02d}')
+        assert result[13:] == '.zip'
+
+
+def test_download_cffex_history_data(path_for_test):
+    """
+    测试 collector.cffex.download_cffex_history_data()。
+
+    download_cffex_history_data() 应生成命名为 <CFFEX_<yyyy>-<mm>.zip> 的新文件，且容量大于 0。
+
+    :param path_for_test: Path，测试固件。测试期间临时文件的路径。
+    :return: None。
+    """
+    for parameter in get_all_cffex_history_data_parameters():
+        year: int = parameter[0]
+        month: int = parameter[1]
+
+        # 应该被下载的文件。
+        file_to_be_downloaded: Path = path_for_test.joinpath(
+            get_cffex_history_data_local_filename(year, month)
+        )
+
+        # 如果已经有了这个文件，删除它。
+        if file_to_be_downloaded.exists():
+            file_to_be_downloaded.unlink()
+
+        # 确保本地不存在这个文件。
+        assert file_to_be_downloaded.exists() is False
+
+        # 下载
+        download_cffex_history_data(path_for_test, year, month)
+
+        # 新文件存在
+        assert file_to_be_downloaded.exists() is True
+
+        # 新文件容量不为 0。
+        assert os.path.getsize(file_to_be_downloaded) > 0
+
+        # 清理文件。
+        file_to_be_downloaded.unlink()
+        assert file_to_be_downloaded.exists() is False
+
+
+def test_read_cffex_history_data(path_for_test):
+    """
+    测试 collector.cffex.read_cffex_history_data()。
+
+    :param path_for_test: Path，测试固件。测试期间临时文件的路径。
+    :return: None。
+    """
+    for parameter in get_all_cffex_history_data_parameters():
+        year: int = parameter[0]
+        month: int = parameter[1]
+
+        # 应该被下载的文件。
+        file_to_be_downloaded: Path = path_for_test.joinpath(
+            get_cffex_history_data_local_filename(year, month)
+        )
+
+        # 如果已经有了这个文件，删除它。
+        if file_to_be_downloaded.exists():
+            file_to_be_downloaded.unlink()
+
+        # 确保本地不存在这个文件。
+        assert file_to_be_downloaded.exists() is False
+
+        # 下载
+        download_cffex_history_data(path_for_test, year, month)
+
+        # Unzip <download_file>.
+        unzip_file_list: List[Path] = list(
+            uncompress_zip_file(file_to_be_downloaded, path_for_test)
+        )
+        csv_file: Path
+        for csv_file in unzip_file_list:
+            assert csv_file.exists() is True
+
+            result: Tuple[List[Dict[str, Any]], List[Dict[str, Any]]] = read_cffex_history_data(csv_file)
+            assert isinstance(result, tuple)
+
+            # 转写变量
+            date_futures: List[Dict[str, Any]] = result[0]
+            date_option: List[Dict[str, Any]] = result[1]
+
+            # 期货
+            for item in date_futures:
+                assert isinstance(item, dict)
+
+                assert isinstance(item['exchange'], str)
+                assert item['exchange'] == 'CFFEX'
+
+                assert isinstance(item['date'], dt.date)
+
+                assert isinstance(item['symbol'], str)
+
+                assert isinstance(item['product'], str)
+                assert len(item['product']) <= 2
+
+                assert isinstance(item['expiration'], str)
+                assert len(item['expiration']) == 4
+
+                assert isinstance(item['previous_settlement'], float)
+                assert isinstance(item['open'], float)
+                assert isinstance(item['high'], float)
+                assert isinstance(item['low'], float)
+                assert isinstance(item['close'], float)
+                assert isinstance(item['settlement'], float)
+                assert isinstance(item['change_on_close'], float)
+                assert isinstance(item['change_on_settlement'], float)
+                assert isinstance(item['volume'], int)
+                assert isinstance(item['amount'], float)
+                assert isinstance(item['open_interest'], int)
+                assert isinstance(item['change_on_open_interest'], int)
+
+            # 期权
+            for item in date_option:
+                assert isinstance(item, dict)
+
+                assert isinstance(item['exchange'], str)
+                assert item['exchange'] == 'CFFEX'
+
+                assert isinstance(item['date'], dt.date)
+
+                assert isinstance(item['symbol'], str)
+
+                assert isinstance(item['product'], str)
+                assert len(item['product']) <= 2
+
+                assert isinstance(item['expiration'], str)
+                assert len(item['expiration']) == 4
+
+                assert isinstance(item['offset'], str)
+                assert len(item['offset']) == 1
+
+                assert isinstance(item['exercise_price'], str)
+
+                assert isinstance(item['previous_settlement'], float)
+                assert isinstance(item['open'], float)
+                assert isinstance(item['high'], float)
+                assert isinstance(item['low'], float)
+                assert isinstance(item['close'], float)
+                assert isinstance(item['settlement'], float)
+                assert isinstance(item['change_on_close'], float)
+                assert isinstance(item['change_on_settlement'], float)
+                assert isinstance(item['volume'], int)
+                assert isinstance(item['amount'], float)
+                assert isinstance(item['open_interest'], int)
+                assert isinstance(item['change_on_open_interest'], int)
+
+            # 清理文件。
+            csv_file.unlink()
+            assert csv_file.exists() is False
+
+        assert file_to_be_downloaded.exists() is False
